dcdab71ac18ed06800ad5b9f7397e8f6dd1f9ce8
omursa <omursahin43@gmail.com>
2019-08-17 16:57:13 -0400

    add pom.xml, source

---
+++ src/test/java/com/cybertek/Batch8.java
@@ -0,0 +1,27 @@
+package com.cybertek;
+
+import com.github.javafaker.Faker;
+
+public class Batch8 {
+	public static void main(String[] args) {
+		
+		
+		
+		Faker faker= new Faker();
+		
+		String creditcard=faker.business().creditCardNumber();
+		
+		String creditcardtype= faker.business().creditCardType();
+		String expdate=faker.business().creditCardExpiry();
+		
+		String Book= faker.book().title();
+		String Book1= faker.book().title();
+		
+		System.out.println(Book);
+		System.out.println(faker.country().capital());
+		
+		
+		
+		
+	}
+}
